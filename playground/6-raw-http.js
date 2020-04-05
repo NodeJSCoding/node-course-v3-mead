1078a5b602295a8358b6055c2d9b28b9faab709a
Mykola Matsko <mykola.matsko@gmail.com>
2020-04-05 10:25:43 +0300

    reques without npm library

---
+++ playground/6-raw-http.js
@@ -0,0 +1,23 @@
+const https = require('https');
+
+const url = 'https://api.darksky.net/forecast/7b9a8e8bdf4c401998d3e9c251292727/40,-75'
+
+const request = https.request(url, (response) => {
+    let data = ''
+
+    response.on('data', (chunk) => {
+        data = data + chunk.toString()
+        console.log(chunk)
+    })
+
+    response.on('end', () => {
+        const body = JSON.parse(data)
+        console.log(body)
+    })
+})
+
+request.on('error', (error) => {
+    console.log('An error', error)
+})
+
+request.end()
